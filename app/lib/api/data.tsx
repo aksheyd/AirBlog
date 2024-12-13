6ad5e9738433dde3b7f7922feb927ec57c106bb8
Akshey D <aksheyd@umich.edu>
2024-12-13 00:21:25 -0500

    api routes finally, 30,000 feet i nthe air man

---
+++ app/lib/api/data.tsx
@@ -0,0 +1,64 @@
+import { InvalidAirport, InvalidIATA, ErrorResponse, TerminalsResponse, AirportData } from '@/app/lib/definitions';
+import { NextResponse } from 'next/server';
+
+export async function FindTerminals(reqIATA: string): Promise<NextResponse<TerminalsResponse | ErrorResponse>> {
+    if (reqIATA.length !== 3) {
+        return NextResponse.json(InvalidIATA, { status: 400 });
+    }
+
+    let terminals: TerminalsResponse | undefined;
+    let IATA: string = correctedString(reqIATA);
+
+    terminals = Airports[IATA];
+
+    if (!terminals) {
+        return NextResponse.json(InvalidAirport, { status: 404 });
+    }
+    
+    return NextResponse.json(terminals, { status: 200 });
+
+}
+const Airports: Record<string, AirportData> = {
+    SFO: {
+        name: 'San Francisco International Airport',
+        terminals: [
+            { id: '1', name: 'Harvey Milk Terminal 1', location: 'North Wing' },
+            { id: '2', name: 'Terminal 2', location: 'East Wing' },
+        ],
+    },
+    LAX: {
+        name: 'Los Angeles International Airport',
+        terminals: [
+            { id: '1', name: 'Terminal 3', location: 'West Wing' },
+            { id: '2', name: 'Tom Bradley International Terminal', location: 'Central Hub' },
+        ],
+    },
+    DTW: {
+        name: 'Detroit Metropolitan Wayne County Airport',
+        terminals: [
+            { id: '1', name: 'McNamara Terminal', location: 'South Wing' },
+            { id: '2', name: 'Evans Terminal', location: 'North Wing' },
+        ],
+    },
+    JFK: {
+        name: 'John F. Kennedy International Airport',
+        terminals: [
+            { id: '1', name: 'Terminal 1', location: 'East Wing' },
+            { id: '2', name: 'Terminal 4', location: 'West Wing' },
+        ],
+    },
+    ORD: {
+        name: 'O\'Hare International Airport',
+        terminals: [
+            { id: '1', name: 'Terminal 1', location: 'North Wing' },
+            { id: '2', name: 'Terminal 5', location: 'International Wing' },
+        ],
+    },
+};
+
+function correctedString(input: string): string {
+    // FIXME: add string input checking for safety
+    let res: string = input.toUpperCase()
+
+    return res
+}
